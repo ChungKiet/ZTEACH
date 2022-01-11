5ad83f5ed989acadb87cfed58dacea8a72856cb3
nghiaump <nghiaump@gmail.com>
2022-01-11 12:19:53 +0700

    init EditPostPage

---
+++ frontend/src/components/pages/EditPostPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import EditPost from '../EditPostPage/EditPost';
+function EditPostPage() {
+    return (
+        <div>
+            <EditPost />
+        </div>
+    )
+}
+
+export default EditPostPage
